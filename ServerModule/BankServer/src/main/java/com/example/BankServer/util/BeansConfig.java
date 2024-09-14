70ebb270a952a141fc699b422ab97b91c55a5bbf
shiva2-svg <wankhadevitthal2411@gmail.com>
2024-09-14 12:28:15 +0530

    Add files via upload
    first

---
+++ ServerModule/BankServer/src/main/java/com/example/BankServer/util/BeansConfig.java
@@ -0,0 +1,30 @@
+package com.example.BankServer.util;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfig {
+
+	private SecurityScheme createAPIKeyScheme() {
+		return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("Bearer");
+	}
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+				.components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+				.info(new Info().title("My REST API FOR BANK SERVER").description("Some custom description of API.")
+						.version("1.0")
+						.contact(new Contact().name("Chethan Kumar S").email("www.chethan6320.com")
+								.url("cks632000@gmail.com"))
+						.license(new License().name("License of API").url("API license URL")));
+	}
+}
